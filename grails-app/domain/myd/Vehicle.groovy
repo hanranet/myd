58d9c9be676e103bae83269ec4cbc242bf64506d
Hanrahan <Thomas.B.Hanrahan@ehi.com>
2021-03-01 10:40:39 -0600

    initial commit

---
+++ grails-app/domain/myd/Vehicle.groovy
@@ -0,0 +1,12 @@
+package myd
+
+class Vehicle {
+    String name
+    String make
+    String model
+    static constraints = {
+        name maxSize: 255
+        make inList: ['Ford', 'Chevrolet', 'Nissan']
+        model nullable: true
+    }
+}
